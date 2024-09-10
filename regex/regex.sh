17604af01e559ebf5f7c786a39becd26d3a896d6
Dylan Packer <packe089@morris.umn.edu>
2024-09-10 10:53:09 -0500

    same as last

--- regex/regex.sh
+++ regex/regex.sh
@@ -3,7 +3,8 @@
 # Corrected sed command for r0_input.txt
 sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r0_input.txt > r0_output.txt
 
-# Original sed commands for other files
+# Corrected sed command for r1_input.txt
 sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r1_input.txt > r1_output.txt
 
+# Corrected sed command for r2_input.txt
 sed -E 's/\* ([a-zA-Z.]+), ([a-zA-Z.]+)\. ([a-zA-Z ]+)/1. \1\n2. \3\n\n1. \2\n2. \3\n/' < r2_input.txt > r2_output.txt
