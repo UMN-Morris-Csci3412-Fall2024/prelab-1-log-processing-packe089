a4d0b3eab3e6d24bd547e4006d0b59550c59b230
Dylan Packer <packe089@morris.umn.edu>
2024-09-10 10:49:37 -0500

    the first 2 test pass need to fix code

--- regex/regex.sh
+++ regex/regex.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-cd regex || exit 1
-
-gcc -o regex regex.sh
-
+# Corrected sed command for r0_input.txt
 sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r0_input.txt > r0_output.txt
 
+# Original sed commands for other files
 sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r1_input.txt > r1_output.txt
 
 sed -E 's/\* ([a-zA-Z.]+), ([a-zA-Z.]+)\. ([a-zA-Z ]+)/1. \1\n2. \3\n\n1. \2\n2. \3\n/' < r2_input.txt > r2_output.txt
