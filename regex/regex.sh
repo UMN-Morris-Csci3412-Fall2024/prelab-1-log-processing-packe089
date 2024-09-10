b81215a51a36117c08bb15e3956a092996dbba14
Dylan Packer <packe089@morris.umn.edu>
2024-09-10 10:26:00 -0500

    making sure regex.sh is in the project

--- regex/regex.sh
+++ regex/regex.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+cd regex || exit 1
+
+gcc -o regex regex.sh
+
 sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r0_input.txt > r0_output.txt
 
 sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r1_input.txt > r1_output.txt
