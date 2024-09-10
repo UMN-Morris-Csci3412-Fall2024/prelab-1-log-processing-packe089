59b2421f5c83fa2c1a7bb0607faa1cb0f05b5f7a
Dylan Packer <packe089@morris.umn.edu>
2024-09-10 11:01:16 -0500

    fixing the second case

--- regex/regex.sh
+++ regex/regex.sh
@@ -3,7 +3,7 @@
 # Corrected sed command for r0_input.txt
 sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r0_input.txt > r0_output.txt
 
-# Corrected sed command for r1_input.txt
+# Adjusted sed command for r1_input.txt
 sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r1_input.txt > r1_output.txt
 
 # Corrected sed command for r2_input.txt
